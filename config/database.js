d19f7f842d6f81169369c5defbd673d188b29454
Maduka Chidimma <maduka.chidimma@gmail.com>
2019-06-25 10:04:19 +0100

    initial commit

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://localhost:27017/meanauth',
+    mongoURI: 'mongodb+srv://hephzibah:hephzibah@meanapp-dev-xysfr.mongodb.net/test?retryWrites=true&w=majority',
     secret: 'yoursecret'
 }
